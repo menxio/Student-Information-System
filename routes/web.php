ef80577d3790453b1f363c7dc2ac212cbf8cbec1
menxio <jayceerubiato@gmail.com>
2025-03-11 00:39:47 +0800

    Admin Auth initialization

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::middleware(['auth', 'isAdmin'])->group(function () {
+    Route::get('admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
